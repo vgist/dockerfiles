8c92bfb8be1943ecfa14a2d8a14520160723a262
Register <registerdedicated+github@gmail.com>
2016-09-18 21:34:26 +0800

    pure-ftpd: update entrypoint.sh

--- pure-ftpd/entrypoint.sh
+++ pure-ftpd/entrypoint.sh
@@ -6,7 +6,10 @@ set -e
 
 [[ "$DEBUG" == "true" ]] && set -x
 
-addgroup -g ${GID} -S ftpuser && adduser -u ${UID} -G ftpuser -h /home/ftpuser -D -s /sbin/nologin ftpuser
+addgroup -g ${GID} -S ftpgroup && adduser -u ${UID} -G ftpgroup -h /home/ftpuser -D -s /sbin/nologin ftpuser
+
+[[ $(stat -c %U /home/ftpuser) == "ftpuser" ]] || chown -R ftpuser /home/ftpuser
+[[ $(stat -c %G /home/ftpuser) == "ftpgroup" ]] || chgrp -R ftpgroup /home/ftpuser
 
 [[ -f /etc/pureftpd/pureftpd.passwd ]] && /usr/bin/pure-pw mkdb
 
