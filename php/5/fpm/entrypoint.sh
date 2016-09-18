d886479717087449025cfc783e27ec0767cc41b7
Register <registerdedicated+github@gmail.com>
2016-09-18 19:59:27 +0800

    php: env uid and gid

--- php/5/fpm/entrypoint.sh
+++ php/5/fpm/entrypoint.sh
@@ -13,19 +13,22 @@ sed -i "s|error_log\s*=\s*/var/log/php-fpm.log|error_log = /proc/self/fd/2|g" /e
 sed -i "s|;*access.log\s*=\s*log/\$pool.access.log|access.log = /proc/self/fd/1|g" /etc/php5/php-fpm.conf
 sed -i "s|;*listen\s*=\s*127.0.0.1:9000|listen = 9000|g" /etc/php5/php-fpm.conf
 sed -i "s|;*chdir\s*=\s*/var/www|chdir = /var/www|g" /etc/php5/php-fpm.conf
-sed -i "s|pm.max_children =.*|pm.max_children = 12|i" /etc/php5/php-fpm.conf
-sed -i "s|pm.start_servers =.*|pm.start_servers = 6|i" /etc/php5/php-fpm.conf
-sed -i "s|pm.min_spare_servers =.*|pm.min_spare_servers = 2|i" /etc/php5/php-fpm.conf
-sed -i "s|pm.max_spare_servers =.*|pm.max_spare_servers = 10|i" /etc/php5/php-fpm.conf
-sed -i "s|;*date.timezone =.*|date.timezone = ${TIMEZONE}|i" /etc/php5/php.ini
-sed -i "s|;*memory_limit =.*|memory_limit = ${PHP_MEMORY_LIMIT}|i" /etc/php5/php.ini
-sed -i "s|;*upload_max_filesize =.*|upload_max_filesize = ${MAX_UPLOAD}|i" /etc/php5/php.ini
-sed -i "s|;*max_file_uploads =.*|max_file_uploads = ${PHP_MAX_FILE_UPLOAD}|i" /etc/php5/php.ini
-sed -i "s|;*post_max_size =.*|post_max_size = ${PHP_MAX_POST}|i" /etc/php5/php.ini
+sed -i "s|pm.max_children =.*|pm.max_children = ${PM_MAX_CHILDREN}|i" /etc/php5/php-fpm.conf
+sed -i "s|pm.start_servers =.*|pm.start_servers = ${PM_START_SERVERS}|i" /etc/php5/php-fpm.conf
+sed -i "s|pm.min_spare_servers =.*|pm.min_spare_servers = ${PM_MIN_SPARE_SERVERS}|i" /etc/php5/php-fpm.conf
+sed -i "s|pm.max_spare_servers =.*|pm.max_spare_servers = ${PM_MAX_SPARE_SERVERS}|i" /etc/php5/php-fpm.conf
+sed -i "s|user =.*|user = phpuser|i" /etc/php5/php-fpm.conf
+sed -i "s|group =.*|group = phpgroup|i" /etc/php5/php-fpm.conf
+sed -i "s|;*date.timezone =.*|date.timezone = ${DATE_TIMEZONE}|i" /etc/php5/php.ini
+sed -i "s|;*memory_limit =.*|memory_limit = ${MEMORY_LIMIT}|i" /etc/php5/php.ini
+sed -i "s|;*upload_max_filesize =.*|upload_max_filesize = ${UPLOAD_MAX_FILESIZE}|i" /etc/php5/php.ini
+sed -i "s|;*max_file_uploads =.*|max_file_uploads = ${MAX_FILE_UPLOADS}|i" /etc/php5/php.ini
+sed -i "s|;*post_max_size =.*|post_max_size = ${POST_MAX_SIZE}|i" /etc/php5/php.ini
 sed -i "s|;\s*max_input_vars =.*|max_input_vars = ${MAX_INPUT_VARS}|i" /etc/php5/php.ini
 sed -i "s|;*cgi.fix_pathinfo=.*|cgi.fix_pathinfo= 0|i" /etc/php5/php.ini
 
 mkdir -p /var/www
-chown -Rf nobody:nobody /var/www
+
+addgroup -g ${GID} -S phpgroup && adduser -u ${UID} -G phpgroup -H -D -s /sbin/nologin phpuser
 
 exec "$@"
