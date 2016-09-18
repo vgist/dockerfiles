b5098e3a9768aaab6bcc81a3a00571603673add7
Register <registerdedicated+github@gmail.com>
2016-09-18 21:28:28 +0800

    nginx: update entrypoint.sh

--- nginx/stable/entrypoint.sh
+++ nginx/stable/entrypoint.sh
@@ -6,14 +6,11 @@ set -e
 
 [[ "$DEBUG" == "true" ]] && set -x
 
-export VOLUME_USER=$(stat -c %U /var/www)
-export VOLUME_GROUP=$(stat -c %G /var/www)
-
 deluser nginx
 addgroup -g ${GID} -S nginx && adduser -u ${UID} -G nginx -H -D -s /sbin/nologin nginx
 mkdir -p /var/www
 
-[[ ${VOLUME_USER} == "nginx" ]] || chown -R nginx /var/www
-[[ ${VOLUME_GROUP} == "nginx" ]] || chgrp -R nginx /var/www
+[[ $(stat -c %U /var/www) == "nginx" ]] || chown -R nginx /var/www
+[[ $(stat -c %G /var/www) == "nginx" ]] || chgrp -R nginx /var/www
 
 exec "$@"
