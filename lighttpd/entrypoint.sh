68ffff592b6a9e7ff8824ecb5a2c2616901fccfc
Register <registerdedicated+github@gmail.com>
2016-09-18 21:31:27 +0800

    lighttpd: update entrypoint.sh

--- lighttpd/entrypoint.sh
+++ lighttpd/entrypoint.sh
@@ -10,4 +10,7 @@ deluser lighttpd
 addgroup -g ${GID} -S lighttpd && adduser -u ${UID} -G lighttpd -H -D -s /sbin/nologin lighttpd
 chown -R lighttpd:lighttpd /var/www
 
+[[ $(stat -c %U /var/www) == "lighttpd" ]] || chown -R lighttpd /var/www
+[[ $(stat -c %G /var/www) == "lighttpd" ]] || chgrp -R lighttpd /var/www
+
 exec "$@"
