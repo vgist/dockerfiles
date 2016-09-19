1061a642fa03cd5228bd92b04132f382f3d61166
Register <registerdedicated+github@gmail.com>
2016-09-19 18:22:02 +0800

    nginx: fix nginx 500 error

--- nginx/stable/entrypoint.sh
+++ nginx/stable/entrypoint.sh
@@ -8,7 +8,7 @@ set -e
 
 deluser nginx
 addgroup -g ${GID} -S nginx && adduser -u ${UID} -G nginx -H -D -s /sbin/nologin nginx
-mkdir -p /var/www
+chown nginx:nginx /var/lib/nginx
 
 [[ $(stat -c %U /var/www) == "nginx" ]] || chown -R nginx /var/www
 [[ $(stat -c %G /var/www) == "nginx" ]] || chgrp -R nginx /var/www
