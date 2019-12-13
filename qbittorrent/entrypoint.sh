65652d1b137e2b3628b499e6c35c41a7b25832d4
Register <registerdedicated+github@gmail.com>
2019-12-13 22:18:13 +0800

    qbittorrent: add

---
+++ qbittorrent/entrypoint.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+#
+# entrypoint.sh
+
+set -e
+
+[[ "$DEBUG" == "true" ]] && set -x
+
+addgroup -g ${GID} qbittorrent
+adduser -h /data -s /bin/sh -G qbittorrent -D -u ${UID} qbittorrent
+
+mkdir -p /data/.config/qBittorrent
+
+[[ ! -f /data/.config/qBittorrent/qBittorrent.conf ]] && cp /etc/qBittorrent.conf /data/.config/qBittorrent/
+
+sed -i "s|Connection\\\PortRangeMin=.*|Connection\\\PortRangeMin=${PEER_PORT}|i" /data/.config/qBittorrent/qBittorrent.conf
+
+chown -R qbittorrent:qbittorrent /data
+
+exec su-exec qbittorrent:qbittorrent "$@"
