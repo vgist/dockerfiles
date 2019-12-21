f43c1f4b140ef8fcbf8039658c47a75e429b1f4e
Register <458892+aieu@users.noreply.github.com>
2019-12-21 12:55:50 +0800

    rtorrent: change homedir to /data

--- rutorrent/rootfs/usr/bin/entrypoint.sh
+++ rutorrent/rootfs/usr/bin/entrypoint.sh
@@ -6,19 +6,17 @@ set -e
 
 [[ "$DEBUG" == "true" ]] && set -x
 
-mkdir -p /data/torrents /data/.watch /data/.session /home/torrent /var/tmp/nginx
+addgroup -g ${GID} torrent && adduser -h /data -s /bin/sh -G torrent -D -u ${UID} torrent
+mkdir -p /data/torrents /data/.watch /data/.session /var/tmp/nginx
 
-[[ -f /home/torrent/.rtorrent.rc ]] || cp /etc/rtorrent.rc /home/torrent/.rtorrent.rc
-[[ -f /home/torrent/scgi.socket ]] || touch /home/torrent/scgi.socket
+[[ -f /data/.rtorrent.rc ]] || cp /etc/rtorrent.rc /data/.rtorrent.rc
+[[ -f /data/scgi.socket ]] || touch /data/scgi.socket
 [[ -f /data/.session/rtorrent.lock ]] && rm -f /data/.session/rtorrent.lock
 
 htpasswd -b -c /var/www/rutorrent/.htpasswd $USERNAME ${PASSWORD:-$(hostname)}
 
-addgroup -g ${GID} torrent && adduser -h /home/torrent -s /bin/sh -G torrent -D -u ${UID} torrent
-
 chown -R torrent:torrent /data
 chown -R torrent:torrent /var/www
-chown -R torrent:torrent /home/torrent
 chown -R torrent:torrent /var/lib/nginx
 chown -R torrent:torrent /var/tmp/nginx
 
