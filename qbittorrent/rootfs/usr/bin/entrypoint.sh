9a014f9822507aac67efa2c166e48020859bd547
Register <458892+aieu@users.noreply.github.com>
2020-04-25 15:10:51 +0800

    qbittorrent: 4.2.5

--- qbittorrent/rootfs/usr/bin/entrypoint.sh
+++ qbittorrent/rootfs/usr/bin/entrypoint.sh
@@ -14,6 +14,7 @@ mkdir -p /data/.config/qBittorrent
 [[ ! -f /data/.config/qBittorrent/qBittorrent.conf ]] && cp /etc/qBittorrent.conf /data/.config/qBittorrent/
 
 sed -i "s|Connection\\\PortRangeMin=.*|Connection\\\PortRangeMin=${PEER_PORT}|i" /data/.config/qBittorrent/qBittorrent.conf
+sed -i "s|WebUI\\\Port=.*|WebUI\\\Port=${WEB_PORT}|i" /data/.config/qBittorrent/qBittorrent.conf
 
 chown -R qbittorrent:qbittorrent /data
 
