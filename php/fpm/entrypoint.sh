b51b61d165aa74fbdd7926864de3cccf066a2c17
clown <458892+aieu@users.noreply.github.com>
2024-12-29 17:46:12 +0800

    php: v8.4

--- php/fpm/entrypoint.sh
+++ php/fpm/entrypoint.sh
@@ -7,26 +7,26 @@ set -e
 [[ "$DEBUG" == "true" ]] && set -x
 
 # Set environments
-sed -i "s|;*daemonize =.*|daemonize = no|g" /etc/php83/php-fpm.conf
-sed -i "s|;*error_log =.*|error_log = /proc/self/fd/2|g" /etc/php83/php-fpm.conf
-sed -i "s|;*access.log =.*|access.log = /proc/self/fd/1|g" /etc/php83/php-fpm.d/www.conf
-sed -i "s|;*listen\s*=\s*127.0.0.1:9000|listen = /run/php/php8-fpm.sock|g" /etc/php83/php-fpm.d/www.conf
-sed -i "s|;*listen.mode\s*=\s*0660|listen.mode = 0666|g" /etc/php83/php-fpm.d/www.conf
-sed -i "s|;*chdir =.*|chdir = /var/www|g" /etc/php83/php-fpm.d/www.conf
-sed -i "s|pm.max_children =.*|pm.max_children = ${PM_MAX_CHILDREN}|i" /etc/php83/php-fpm.d/www.conf
-sed -i "s|pm.start_servers =.*|pm.start_servers = ${PM_START_SERVERS}|i" /etc/php83/php-fpm.d/www.conf
-sed -i "s|pm.min_spare_servers =.*|pm.min_spare_servers = ${PM_MIN_SPARE_SERVERS}|i" /etc/php83/php-fpm.d/www.conf
-sed -i "s|pm.max_spare_servers =.*|pm.max_spare_servers = ${PM_MAX_SPARE_SERVERS}|i" /etc/php83/php-fpm.d/www.conf
-sed -i "s|user =.*|user = phpuser|i" /etc/php83/php-fpm.d/www.conf
-sed -i "s|group =.*|group = phpgroup|i" /etc/php83/php-fpm.d/www.conf
-sed -i "s|;*date.timezone =.*|date.timezone = ${DATE_TIMEZONE}|i" /etc/php83/php.ini
-sed -i "s|;*memory_limit =.*|memory_limit = ${MEMORY_LIMIT}|i" /etc/php83/php.ini
-sed -i "s|;*max_execution_time =.*|max_execution_time = ${MAX_EXECUTION_TIME}|i" /etc/php83/php.ini
-sed -i "s|;*upload_max_filesize =.*|upload_max_filesize = ${UPLOAD_MAX_FILESIZE}|i" /etc/php83/php.ini
-sed -i "s|;*max_file_uploads =.*|max_file_uploads = ${MAX_FILE_UPLOADS}|i" /etc/php83/php.ini
-sed -i "s|;*post_max_size =.*|post_max_size = ${POST_MAX_SIZE}|i" /etc/php83/php.ini
-sed -i "s|;*max_input_vars =.*|max_input_vars = ${MAX_INPUT_VARS}|i" /etc/php83/php.ini
-sed -i "s|;*cgi.fix_pathinfo=.*|cgi.fix_pathinfo= 0|i" /etc/php83/php.ini
+sed -i "s|;*daemonize =.*|daemonize = no|g" /etc/php84/php-fpm.conf
+sed -i "s|;*error_log =.*|error_log = /proc/self/fd/2|g" /etc/php84/php-fpm.conf
+sed -i "s|;*access.log =.*|access.log = /proc/self/fd/1|g" /etc/php84/php-fpm.d/www.conf
+sed -i "s|;*listen\s*=\s*127.0.0.1:9000|listen = /run/php/php8-fpm.sock|g" /etc/php84/php-fpm.d/www.conf
+sed -i "s|;*listen.mode\s*=\s*0660|listen.mode = 0666|g" /etc/php84/php-fpm.d/www.conf
+sed -i "s|;*chdir =.*|chdir = /var/www|g" /etc/php84/php-fpm.d/www.conf
+sed -i "s|pm.max_children =.*|pm.max_children = ${PM_MAX_CHILDREN}|i" /etc/php84/php-fpm.d/www.conf
+sed -i "s|pm.start_servers =.*|pm.start_servers = ${PM_START_SERVERS}|i" /etc/php84/php-fpm.d/www.conf
+sed -i "s|pm.min_spare_servers =.*|pm.min_spare_servers = ${PM_MIN_SPARE_SERVERS}|i" /etc/php84/php-fpm.d/www.conf
+sed -i "s|pm.max_spare_servers =.*|pm.max_spare_servers = ${PM_MAX_SPARE_SERVERS}|i" /etc/php84/php-fpm.d/www.conf
+sed -i "s|user =.*|user = phpuser|i" /etc/php84/php-fpm.d/www.conf
+sed -i "s|group =.*|group = phpgroup|i" /etc/php84/php-fpm.d/www.conf
+sed -i "s|;*date.timezone =.*|date.timezone = ${DATE_TIMEZONE}|i" /etc/php84/php.ini
+sed -i "s|;*memory_limit =.*|memory_limit = ${MEMORY_LIMIT}|i" /etc/php84/php.ini
+sed -i "s|;*max_execution_time =.*|max_execution_time = ${MAX_EXECUTION_TIME}|i" /etc/php84/php.ini
+sed -i "s|;*upload_max_filesize =.*|upload_max_filesize = ${UPLOAD_MAX_FILESIZE}|i" /etc/php84/php.ini
+sed -i "s|;*max_file_uploads =.*|max_file_uploads = ${MAX_FILE_UPLOADS}|i" /etc/php84/php.ini
+sed -i "s|;*post_max_size =.*|post_max_size = ${POST_MAX_SIZE}|i" /etc/php84/php.ini
+sed -i "s|;*max_input_vars =.*|max_input_vars = ${MAX_INPUT_VARS}|i" /etc/php84/php.ini
+sed -i "s|;*cgi.fix_pathinfo=.*|cgi.fix_pathinfo= 0|i" /etc/php84/php.ini
 
 mkdir -p /var/www
 
